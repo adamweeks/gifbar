758ed9f369b2dbf5ca2fa07ff39f4ea14460ad02
Adam Weeks <aweeks@about.com>
2016-06-07 15:40:08 -0400

    Electron packager support

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 1200, height: 900});
+  win = new BrowserWindow({width: 440, height: 600});
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`);
