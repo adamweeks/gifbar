f4e0df44d97b13d876071e5578e9a1169b509347
Adam Weeks <aweeks@about.com>
2016-06-15 16:32:03 -0400

    Releases added to the repo

---
+++ releases/gifbar-win32-x64/resources/app/electron.js
@@ -0,0 +1,11 @@
+webpackJsonp([2,3],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	__webpack_require__(559);
+	module.exports = __webpack_require__(563);
+
+
+/***/ }
+]);
+//# sourceMappingURL=electron.js.map
