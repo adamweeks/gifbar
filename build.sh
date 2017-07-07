6441eb57e9bd2021ec96278b881fcd067d560afc
Ryan Martinsen <rmartinsen@about.com>
2017-07-07 11:21:54 -0600

    Update build to work with auto launcher.
    * More `node_modules` are necessary to run auto launcher.
    * Build script in npm scripts section was getting too long; break it up into its own bash script (not windows friendly, but the npm script wasn't either).

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir build
+
+rm -rf build/*
+cp -r src/* build/
+
+mkdir build/node_modules
+cp -a node_modules/applescript build/node_modules/
+cp -a node_modules/auto-launch build/node_modules/
+cp -a node_modules/electron-positioner build/node_modules/
+cp -a node_modules/extend build/node_modules/
+cp -a node_modules/mkdirp build/node_modules/
+cp -a node_modules/path-is-absolute build/node_modules/
+cp -a node_modules/untildify build/node_modules/
+cp -a node_modules/winreg build/node_modules/
+
+npm run build-webpack
