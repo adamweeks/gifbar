f48385ed0e54a6edabcc41c409be63348a545867
Adam Weeks <adweeks@cisco.com>
2017-08-11 16:19:35 -0400

    fix: update build packaging process

--- build.sh
+++ build.sh
@@ -6,14 +6,21 @@ rm -rf build/*
 cp -r src/* build/
 
 mkdir build/node_modules
+# TODO: Get these packages from app/package.json
 cp -a node_modules/applescript build/node_modules/
 cp -a node_modules/auto-launch-patched build/node_modules/
 cp -a node_modules/electron-positioner build/node_modules/
 cp -a node_modules/electron-settings build/node_modules/
 cp -a node_modules/extend build/node_modules/
+cp -a node_modules/growly build/node_modules/
+cp -a node_modules/isexe build/node_modules/
 cp -a node_modules/mkdirp build/node_modules/
+cp -a node_modules/node-notifier build/node_modules/
 cp -a node_modules/path-is-absolute build/node_modules/
+cp -a node_modules/semver build/node_modules/
+cp -a node_modules/shellwords build/node_modules/
 cp -a node_modules/untildify build/node_modules/
+cp -a node_modules/which build/node_modules/
 cp -a node_modules/winreg build/node_modules/
 
 npm run build-webpack
