c96fa5a280b3da2d5ab7201391e1bac69bbae769
Adam Weeks <adweeks@cisco.com>
2017-11-20 17:22:37 -0500

    feat(image-actions): add handle remove favorite

--- src/app/components/image-actions/index.jsx
+++ src/app/components/image-actions/index.jsx
@@ -2,17 +2,25 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './styles.css';
-import copyIcon from '../../images/Copy-128.png';
-import starIcon from '../../images/Shape-Star2-128.png';
+import copyIcon from './Copy-128.png';
+import starIcon from './Shape-Star2-128.png';
+import trashIcon from './Garbage-Closed-128.png';
 
 class ImageActions extends Component {
   render() {
-    const {handleCopy, handleFavorite} = this.props;
+    const {handleCopy, handleFavorite, handleRemoveFavorite, showFavorite, showRemoveFavorite} = this.props;
     return (
       <div className={styles.actionItems}>
-        <button className={styles.button} onClick={handleFavorite} title='Click to favorite'>
-          <img className={styles.image} src={starIcon} />
-        </button>
+        {showFavorite &&
+          <button className={styles.button} onClick={handleFavorite} title='Click to favorite'>
+            <img className={styles.image} src={starIcon} />
+          </button>
+        }
+        {showRemoveFavorite &&
+          <button className={styles.button} onClick={handleRemoveFavorite} title='Remove from favorite'>
+            <img className={styles.image} src={trashIcon} />
+          </button>
+        }
         <button className={styles.button} onClick={handleCopy} title='Click to copy the Url'>
           <img className={styles.image} src={copyIcon} />
         </button>
@@ -23,7 +31,10 @@ class ImageActions extends Component {
 
 ImageActions.propTypes = {
   handleCopy: PropTypes.func.isRequired,
-  handleFavorite: PropTypes.func.isRequired
+  handleFavorite: PropTypes.func,
+  handleRemoveFavorite: PropTypes.func,
+  showFavorite: PropTypes.bool,
+  showRemoveFavorite: PropTypes.bool
 };
 
 export default ImageActions;
