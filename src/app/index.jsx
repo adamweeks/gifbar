a1368f2dbec573c8534073145be1ae9ac7f2e80f
Adam Weeks <me@adamweeks.com>
2018-02-20 21:13:25 -0500

    fix: devtools install to main

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,7 +3,4 @@ import ReactDOM from 'react-dom';
 
 import Main from './containers/main';
 
-import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
-installExtension(REACT_DEVELOPER_TOOLS);
-
 ReactDOM.render(<Main />, document.getElementById(`main`));
