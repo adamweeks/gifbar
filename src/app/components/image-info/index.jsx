e887e7c715999475bc0ee781a3ea4e36927f730e
Adam Weeks <me@adamweeks.com>
2018-02-24 17:53:41 -0500

    feat(image-info): convert to rnw

--- src/app/components/image-info/index.jsx
+++ src/app/components/image-info/index.jsx
@@ -1,16 +1,34 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import {Text, View} from 'react-native';
 
-import styles from './styles.css';
-
-class ImageInfo extends Component {
-  render() {
-    return (
-      <div className={styles.info}>
-        { this.props.info }
-      </div>
-    );
+const styles = {
+  info: {
+    backgroundColor: 'white',
+    fontFamily: 'Helvetica Neue',
+    fontSize: 20,
+    height: 22,
+    lineHeight: 20,
+    opacity: 0.6,
+    paddingLeft: 5,
+    paddingRight: 5
+  },
+  imageInfo: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0
   }
+};
+
+
+function ImageInfo({info}) {
+  return (
+    <View style={styles.imageInfo}>
+      <Text style={styles.info}>
+        { info }
+      </Text>
+    </View>
+  );
 }
 
 ImageInfo.propTypes = {
