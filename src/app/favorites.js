6c9a115c32291f85c10777c2975a2e165da38516
Ryan Martinsen <ryan@ryanware.com>
2019-05-02 15:19:44 -0400

    Adds pagination support for favorites (#170)

--- src/app/favorites.js
+++ src/app/favorites.js
@@ -22,7 +22,10 @@ export function storeFavorite(image) {
 }
 
 export function getFavorites(amount=20, offset=0) {
-  return favorites.slice(offset, offset+amount);
+  return {
+    favorites: favorites.slice(offset, offset+amount),
+    totalCount: favorites.length,
+  };
 }
 
 export function removeFavorite(image) {
